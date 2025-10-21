e4eb9928bfe5ccb1451c31196fc19ffbfa246eec
Rodrigo Fernandes <rodrigo.fernandes@datadoghq.com>
2025-10-21 15:07:17 +0100

    updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,3 +14,19 @@ source ~/.config/fish/init.fish
 if type oh-my-posh &>/dev/null
     oh-my-posh init fish --config ~/.config/omp/rtfpessoa.omp.json | source
 end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/opt/homebrew/share/google-cloud-sdk/path.fish.inc' ]; . '/opt/homebrew/share/google-cloud-sdk/path.fish.inc'; end
+
+# Toggle an Oh My Posh segment twice (forces a re-render) safely.
+function __omp_reset
+    oh-my-posh init fish --config ~/.config/omp/rtfpessoa.omp.json | source
+end
+
+# Invalidate OMP cache after switching K8s context/namespace
+# Triggered after every command that runs in the shell.
+function __omp_clear_cache_after_kctx --on-event fish_postexec --argument-names cmd _status
+    if string match -rq '^(ddtool|kubectl|k|kubectx|kctx)\s.+' -- $cmd
+        __omp_reset
+    end
+end
--- vim/.config/nvim/lua/plugins/github.lua
+++ vim/.config/nvim/lua/plugins/github.lua
@@ -1,5 +1,5 @@
 return {
-  "almm7aya/openingh.nvim",
+  "Almo7aya/openingh.nvim",
   keys = {
     { "<leader>ghl", "<cmd>OpenInGHFileLines<cr>", desc = "Open in Github" },
   },
